98402bb5222bc6e76d0c45a82b525751b0cbcf05
cybertron <martadeen01@gmail.com>
2023-02-21 17:26:11 -0800

    no.2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,16 +1,13 @@
 /*
  * File: 2-print_alphabet_x10.c
  * Auth: Brennan D Baraban
- * print_alphabet_x10 - Prints 10 times the alphabet, in lowercase
- *
 */
 
 #include "main.h"
 
-/*
+/**
  * print_alphabet_x10 - Prints 10 times the alphabet, in lowercase.
 */
-
 void print_alphabet_x10(void)
 {
 	int count = 0;
