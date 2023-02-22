d7cf1866f342e05db5d90c5d03356118afafcbc3
cybertron <martadeen01@gmail.com>
2023-02-21 16:40:04 -0800

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*                                              * main - The entry point of the program
+ *                                              * Return: will return 0 when the peogram is successful
+ *
+*/
+
+int main(void)
+{
+        printf("_putchar\n");                          return (0);
+}
