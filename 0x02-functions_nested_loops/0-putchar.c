7413a2f71c7acc779c528a6e4b32d1a67591b639
cybertron <martadeen01@gmail.com>
2023-02-21 16:47:06 -0800

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,8 @@
 #include "main.h"
 #include <stdio.h>
-/*                                              * main - The entry point of the program
- *                                              * Return: will return 0 when the peogram is successful
- *
+/*                                              * main: check the code
+ *                                              * Return: will return 0 if success
+ *                                              *
 */
 
 int main(void)
