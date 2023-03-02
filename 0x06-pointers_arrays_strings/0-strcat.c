2634020638526dc57df2cc04abdad77c94f62d9f
cybertron <martadeen01@gmail.com>
2023-03-01 17:44:02 -0800

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,21 +7,13 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i;
-	int j;
-
+	int i, j;
 	i = 0;
-	while (dest[i] != '\0')
+	for (j = 0; dest[j] != '\0'; j++);
+	while (src[i] != '\0')
 	{
+		dest[j + i] = src[i];
 		i++;
 	}
-	j = 0;
-	while (src[j] != '\0')
-	{
-		dest[j] = src[i];
-		i++;
-		j++;
-	}
-	dest[i] = '\0';
 	return (dest);
 }
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,5 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
 
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
@@ -14,5 +12,3 @@ void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 int _putchar(char c);
-
-#endif
