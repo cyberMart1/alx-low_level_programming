f6c4ef9463e29d8339f14ac74a77c5dd65303944
cybertron <martadeen01@gmail.com>
2023-03-02 17:08:06 -0800

    no .7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+/**
+ * leet - encodes a string into leetspeek.
+ * @s: pointer string.
+ * @n: pointer to input string.
+ * Return: Returns pointer to leetspeek string.
+ */
+
+char *leet(char *n)
+{
+	int i, j;
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
+
+	for (i = 0; n[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (n[i] == s1[j])
+			{
+				n[i] = s2[j];
+			}
+		}
+	}
+	return (n);
+}
+
