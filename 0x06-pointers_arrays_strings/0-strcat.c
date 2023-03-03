e7e563e6a37f07dc36c3196d0f8482b429a6defd
cybertron <martadeen01@gmail.com>
2023-03-02 16:08:15 -0800

    no.1

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -5,4 +5,15 @@
  *    * @src: pointer to the source string
  *     * Return: pointer to the resulting string dest
  */
-
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+	i = 0;
+	for (j = 0; dest[j] != '\0'; j++);
+	while (src[i] != '\0')
+	{
+		dest[j + i] = src[i];
+		i++;
+	}
+	return (dest);
+}
