972dbda4d9be65cfbfc067641319045de0eee100
cybertron <martadeen01@gmail.com>
2023-03-02 16:42:13 -0800

    no. 2

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -4,7 +4,7 @@
  *  * @src: pointer to the source string
  *   * @n: number of bytes to print
  *    * Return: pointer to the resulting string dest
- *     */
+ */
 char *_strncat(char *dest, char *src, int n)
 {
 	int i;
