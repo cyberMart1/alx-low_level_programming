4dbc38b1199f63ff7643206651525225f2921128
cybertron <martadeen01@gmail.com>
2023-03-06 11:49:50 -0800

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,9 +1,9 @@
 /**
- * _strchr - locates a character in a string
- * @s: pointer to string
- * @c: character to locate
- * Return: pointer to first occurence of character c
- * or NULL if character is not found.
+ *  * _strchr - locates a character in a string
+ *   * @s: pointer to string
+ *    * @c: character to locate
+ *     * Return: pointer to first occurence of character c
+ *      * or NULL if character is not found.
  */
 
 char *_strchr(char *s, char c)
@@ -11,14 +11,10 @@ char *_strchr(char *s, char c)
 	while (*s != '\0')
 	{
 		if (*s == c)
-		{
 			return (s);
-		}
 		else if (*(s + 1) == c)
-		{
 			return (s + 1);
-			s++;
-		}
+		s++;
 	}
 	return (s + 1);
 }
