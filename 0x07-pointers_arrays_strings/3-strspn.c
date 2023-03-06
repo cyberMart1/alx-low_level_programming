9f7ab68941e17cbd3697d4f0e8dd6b416e6ded4c
cybertron <martadeen01@gmail.com>
2023-03-06 12:31:55 -0800

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,8 +1,8 @@
 /**
- *  * _strspn - gets the length of a prefix substring
- *   * @s: segment to compare bytes from
- *    * @accept: string of bytes to compare with
- *     * Return: number of bytes in segment s which consist of bytes from accept
+ * _strspn - gets the length of a prefix substring
+ * @s: segment to compare bytes from
+ * @accept: string of bytes to compare with
+ * Return: number of bytes in segment s which consist of bytes from accept
  */
 
 unsigned int _strspn(char *s, char *accept)
@@ -14,13 +14,9 @@ unsigned int _strspn(char *s, char *accept)
 	{
 		j = 0;
 		while (accept[j] != '\0' && s[i] != accept[j])
-		{
 			j++;
-		}
 		if (accept[j] == '\0')
-		{
 			return (i);
-		}
 		i++;
 	}
 	return (i);
